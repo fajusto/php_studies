87c46564c97031159f72d17181d70e96c0fa12fa
Fabrício Justo <fajusto@icloud.com>
2015-09-30 16:02:36 -0300

    Função adicionada

--- index.php
+++ index.php
@@ -5,3 +5,14 @@ echo "Hello world! <br>";
 echo "Fabrício Justo <br>";
 
 echo "Raphael Paulino";
+
+function percorre ($quilometros)
+{
+	static $total;
+	$total += $quilometros;
+	echo "percorreu mais $quilometros do total de $total\n<br>";
+}
+percorre(50);
+percorre(100);
+percorre(200);
+percorre(50);
